757c311c8969a6335c03e8cfe54f8c46fa71d6ab
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2023-04-24 11:38:00 -0600

    Cambios en los codigos

--- Divide_And_Conquer.py
+++ Divide_And_Conquer.py
@@ -1,40 +1,28 @@
-# Python code to implement the
-# matrix chain multiplication using recursion
-
 import sys
 
-# Matrix A[i] has dimension p[i-1] x p[i]
-# for i = 1..n
 def MatrixChainOrder(p, i, j):
-    if i == j:
+    if i == j:  # Caso base: no hay multiplicación necesaria
         return 0
 
-    _min = sys.maxsize
+    _min = sys.maxsize  # Inicializa el mínimo número de multiplicaciones
 
-    # Place parenthesis at different places
-    # between first and last matrix,
-    # recursively calculate count of multiplications
-    # for each parenthesis placement
-    # and return the minimum count
+    # Itera sobre todas las posiciones posibles para colocar un paréntesis (Divide)
     for k in range(i, j):
-
+        # Resuelve subproblemas de manera recursiva (Conquer)
         count = (
             MatrixChainOrder(p, i, k)
             + MatrixChainOrder(p, k + 1, j)
-            + p[i - 1] * p[k] * p[j]
+            + p[i - 1] * p[k] * p[j]  # Calcula el costo adicional de multiplicar las matrices
         )
 
-        if count < _min:
+        if count < _min:  # Actualiza el mínimo número de multiplicaciones si es necesario (Combine)
             _min = count
 
-    # Return minimum count
-    return _min
+    return _min  # Retorna el mínimo número de multiplicaciones
 
-
-# Driver code
 if __name__ == "__main__":
-    arr = [1, 2, 3, 4, 3]
+    arr = [1, 2, 3, 4, 3]  # Lista de dimensiones de matrices
     N = len(arr)
 
-    # Function call
-    print("Minimum number of multiplications is ", MatrixChainOrder(arr, 1, N - 1))
+    # Llama a la función MatrixChainOrder y muestra el resultado
+    print("Cantidad minima de multiplicaiones ", MatrixChainOrder(arr, 1, N - 1))
--- dinamica.py
+++ dinamica.py
@@ -1,18 +1,18 @@
-# Python program using memoization
 import sys
 
+# Tabla para almacenar resultados previamente calculados (memoización)
 dp = [[-1 for i in range(100)] for j in range(100)]
 
-# Function for matrix chain multiplication
 def matrixChainMemoised(p, i, j):
-    if i == j:
+    if i == j:  # Caso base: no hay multiplicación necesaria
         return 0
 
-    if dp[i][j] != -1:
+    if dp[i][j] != -1:  # Si el resultado ya ha sido calculado, retornarlo
         return dp[i][j]
 
-    dp[i][j] = sys.maxsize
+    dp[i][j] = sys.maxsize  # Inicializa el mínimo número de multiplicaciones
 
+    # Itera sobre todas las posiciones posibles para colocar un paréntesis
     for k in range(i, j):
         dp[i][j] = min(
             dp[i][j],
@@ -21,18 +21,14 @@ def matrixChainMemoised(p, i, j):
             + p[i - 1] * p[k] * p[j],
         )
 
-    return dp[i][j]
-
+    return dp[i][j]  # Retorna el mínimo número de multiplicaciones
 
 def MatrixChainOrder(p, n):
     i = 1
     j = n - 1
-    return matrixChainMemoised(p, i, j)
+    return matrixChainMemoised(p, i, j)  # Llama a la función con memoización
 
-
-# Driver Code
-arr = [1, 2, 3, 4]
+# Código del controlador
+arr = [1, 2, 3, 4, 3]  # Lista de dimensiones de matrices
 n = len(arr)
-print("Minimum number of multiplications is", MatrixChainOrder(arr, n))
-
-# This code is contributed by rag2127
+print("Cantidad minima de multiplicaiones ", MatrixChainOrder(arr, n))
